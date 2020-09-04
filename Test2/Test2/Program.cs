d3ca31d27b86718f4f2bc294e357544f36005ad6
Sylvain Luiset <sylvain.luiset@saintmichelannecy.fr>
2020-09-04 09:34:31 +0200

    2eme commit

--- Test2/Test2/Program.cs
+++ Test2/Test2/Program.cs
@@ -8,6 +8,9 @@ namespace Test2
         {
             Console.WriteLine("LE J C'EST LE S!");
             Console.WriteLine("J'sort le RS");
+            int jul = 30;
+            jul = jul * 3;
+            Console.WriteLine(jul);
         }
     }
 }
