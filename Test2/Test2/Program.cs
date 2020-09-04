5e8aeb0b08fa36fcaf5a986cff8831af53694df7
Sylvain Luiset <sylvain.luiset@saintmichelannecy.fr>
2020-09-04 09:05:38 +0200

    Initi

---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LE J C'EST LE S!");
+            Console.WriteLine("J'sort le RS");
+        }
+    }
+}
